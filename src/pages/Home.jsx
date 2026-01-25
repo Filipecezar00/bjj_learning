d03555a68860433021293356e095c2ed7fdc88f6
Filipe Cezar <cezarfilipe01@gmail.com>
2026-01-25 14:09:19 -0300

    atualizando

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,12 @@
 // Criando importações da página 
 import{Card,CardContent} from "@/components/ui/card"; 
 import {Button} from "@/components/ui/button"; 
+
+// Criando estrutura de objetos 
+const categories = [
+    {id:1,name:"Guardas"}, 
+    {id:2,name:"Passagens"}, 
+    {id:3,name:"Finalizações"}, 
+    {id:4,name:"Defesa"}, 
+    {id:5,name:"Quedas"} 
+]
