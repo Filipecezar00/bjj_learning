d7f130f823ca9e77c5bcef5222b44835ad6287cf
Filipe Cezar <cezarfilipe01@gmail.com>
2026-01-25 16:09:07 -0300

    atualizando

--- jiu-jitsu-study/src/pages/Home.jsx
+++ jiu-jitsu-study/src/pages/Home.jsx
@@ -14,10 +14,10 @@ const categories = [
 
 // Função com elementos e interface do Usuario 
 return(
-        <div style={{display:"flex", flexDirection:"column", padding:"40px 20px", justifyContent:"center",alignItems:"center",minHeight:"100vh",}}>
-            <div style={{padding:"40px",borderRadius:"12px", textAlign:"center"}}>
-                <h1 style={{fontFamily:"sans-serif",marginBottom:"30px",textAlign:"center",fontSize:"2rem"}}>Estudos de Jiu-jitsu</h1>
-                <div style={{display:"flex",gap:"12px",marginTop:"5px", flexDirection:"row",justifyContent:"center",alignItems:"center",fontFamily:"emoji",fontWeight:"bolder",flexWrap:"wrap",maxWidth:"800px"}}>
+        <div style={{display:"flex", flexDirection:"column", padding:"40px 20px", justifyContent:"center",alignItems:"center",minHeight:"100vh", }}>
+            <div style={{padding:"40px",borderRadius:"12px", textAlign:"center", marginBottom:"100%"}}>
+                <h1 style={{fontFamily:"sans-serif",marginBottom:"30px",textAlign:"left",fontSize:"2rem"}}>Estudos de Jiu-jitsu</h1>
+                <div style={{display:"flex",gap:"12px",marginTop:"5px", flexDirection:"column",justifyContent:"center",alignItems:"flex-start",fontFamily:"sans-serif",fontWeight:"bolder",flexWrap:"nowrap",maxWidth:"800px"}}>
                     {categories.map((cat)=>(
                         <button key={cat}>{cat}</button>
                     ))}
