638059eaf238bfba9fdf06a37087449993ca5abf
Filipe Cezar <cezarfilipe01@gmail.com>
2026-01-30 11:02:19 -0300

    atualizando

--- jiu-jitsu-study/src/routes/chat.routes.js
+++ jiu-jitsu-study/src/routes/chat.routes.js
@@ -1,8 +1,12 @@
+// Importações
 import {Router} from "express"; 
 import {askChatbot} from "../controllers/chat.controller"; 
 
+// variavel da chamada de função
 const router = Router(); 
 
+// Rota para armazenar os dados
 router.post("/",askChatbot); 
 
+// exportando a rota para as demais partes do programa 
 export default router; 
