600ca47f7ce6a8a3dd10d4fa2f3afc4c38baf6dd
Filipe Cezar <cezarfilipe01@gmail.com>
2026-01-30 10:55:19 -0300

    atualizando

--- jiu-jitsu-study/src/server.js
+++ jiu-jitsu-study/src/server.js
@@ -1,14 +1,19 @@
+// Importações
 import express from "express"; 
 import cors from "cors"; 
 import chatRoutes from "./routes/chat.routes.js"; 
 
+// Chamando a função
 const app = express(); 
 
+// Middlewares
 app.use(cors()); 
 app.use(express.json()); 
 
+// Rota do chat
 app.use("/chat",chatRoutes); 
 
+// Chamada do Servidor  
 app.listen(3000,()=>{
     console.log("Servidor rodando")
 })
