8e1161fb8d4329e57e1ebda56e14d69352352e47
Filipe Cezar <cezarfilipe01@gmail.com>
2026-02-01 14:37:07 -0300

    atualizando

--- jiu-jitsu-study/src/pages/Home.jsx
+++ jiu-jitsu-study/src/pages/Home.jsx
@@ -22,7 +22,7 @@ async function handleAsk(video){
 
 setLoading(true)
 
-const response = await fetch("http://localhost:5173/chat",{
+const response = await fetch("http://localhost:3000/chat",{
     method:"POST",
     headers:{"Content-Type":"application/json"}, 
     body:JSON.stringify({
