d9d80aa559914e5451899d9eed45eaa18d3a27d5
Filipe Cezar <cezarfilipe01@gmail.com>
2026-02-01 15:09:44 -0300

    atualizando

--- jiu-jitsu-study/src/controllers/chat.controller.js
+++ jiu-jitsu-study/src/controllers/chat.controller.js
@@ -1,12 +1,18 @@
-import {chatbotService} from "../services/chatbot.service"; 
+import {askAI} from "../services/chatbot.service"; 
 
-export function askChatbot(req,res){
-const {question,video} = req.body 
+export async function askChatbot(req,res){
+    try{
 
-if(!question || !video){
-    return res.status(400).json({error:"Dados não encontrados"}) 
-}
-const answer = chatbotService(question,video); 
- 
-res.json({answer}) 
+        const {question,video} = req.body 
+
+        if(!question|| !video){
+            return res.status(400).json({error:"Dados Inválidos ou Incompletos"})
+        }
+        
+        const answer = await askAI(question,video) ; 
+        res.json({answer}); 
+    }catch(error){
+        console.log(error)
+        res.status(500).json({error:"Servidor instável"})  
+    }
 }
