724073010f70fff919bc77d604fe1fd746ed027a
Filipe Cezar <cezarfilipe01@gmail.com>
2026-02-02 10:00:54 -0300

    tentando debugar

--- frontend/src/components/VideoCard.jsx
+++ frontend/src/components/VideoCard.jsx
@@ -1,4 +1,4 @@
-export default function VideoCard({title,level,onclick}){
+export  function VideoCard({title,level,onclick}){
     return(
            <div onClick={onclick} style={{border:"1px solid #ffffff",padding:"12px",borderRadius:"6px",marginBottom:"8px", cursor:"pointer"}}>
                 <h4>{title}</h4>
