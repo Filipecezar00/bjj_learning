e34ee34efa90217218d7b4b35cd35811a8804e21
Filipe Cezar <cezarfilipe01@gmail.com>
2026-02-03 19:35:38 -0300

    atualizando

---
+++ frontend/src/services/chatbotService.js
@@ -0,0 +1,21 @@
+const API_URL  = "http://localhost:3000"
+
+export async function askChatbot(question,video){
+    try{
+        const response = await fetch(`${API_URL}/chatbot`,{
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json", 
+            }, 
+            body:JSON.stringify({question,video}), 
+        }); 
+        if(!response.ok){
+            throw new Error("Erro ao consultar o chatbot"); 
+        }
+        const data = await response.json(); 
+        return data.answer; 
+    }catch(error){
+        console.error("ERRO NO CHATBOT: " + error) 
+        throw error; 
+    }
+}
