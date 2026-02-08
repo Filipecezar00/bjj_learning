98092d29b27eaf70261817be75615a7bd078aea7
Filipe Cezar <cezarfilipe01@gmail.com>
2026-02-08 20:06:37 -0300

    atualizando

--- backend/src/services/groq.service.js.js
+++ backend/src/services/groq.service.js.js
@@ -1,1 +1,28 @@
+import Groq from "groq-sdk"; 
 
+const groq = new Groq({
+    apiKey: process.env.GROQ_API_KEY 
+}); 
+
+export async function askGroq(prompt){
+    if(!process.env.GROQ_API_KEY){
+         console.error("A chave da Api não foi encontrada"); 
+        throw new Error("GROQ_API_KEY não encontrada") 
+    }
+    const completion = await groq.chat.completions.create({
+        model:"llama3-8b-8192", 
+        messages:[
+            {
+                role:"system", 
+                content:"Você é um treinador de jiu-jitsu didatico e com pouca paciência" 
+            }, 
+
+            {
+                role:"user", 
+                content:prompt 
+            }
+        ],
+        temperature:0.7 
+    }); 
+    return completion.choices[0].message.content; 
+}
