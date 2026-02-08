6be1cd2e2dce0a06019adbcce34e98c22955ac01
Filipe Cezar <cezarfilipe01@gmail.com>
2026-02-08 20:50:22 -0300

    atualizando

--- backend/src/services/groq.service.js
+++ backend/src/services/groq.service.js
@@ -13,7 +13,7 @@ export async function askGroq(prompt){
     }); 
 
     const completion = await groq.chat.completions.create({
-        model:"llama3-8b-8192", 
+        model:"llama-3.1-8b-instant", 
         messages:[
             {
                 role:"system", 
