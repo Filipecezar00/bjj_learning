bd31581f6bbb10fc046cbeeaf9ccf8008069b355
Filipe Cezar <cezarfilipe01@gmail.com>
2026-02-08 20:28:07 -0300

    atualizando

--- backend/src/routes/chat.routes.js
+++ backend/src/routes/chat.routes.js
@@ -1,14 +1,12 @@
 // Importações
 import {Router} from "express"; 
-import {askChatbot} from "../controllers/chat.controller.js"; 
+import {chat} from "../controllers/chat.controller.js"; 
 
 // variavel da chamada de função
 const router = Router(); 
  
-
-
 // Rota para armazenar os dados
-router.post("/",askChatbot); 
+router.post("/chat",chat); 
 
 // exportando a rota para as demais partes do programa 
 export default router; 
