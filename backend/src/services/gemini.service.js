f3fd858c4a03d10478812782d29057604f6842dc
Filipe Cezar <cezarfilipe01@gmail.com>
2026-02-08 17:49:44 -0300

    atualizando

--- backend/src/services/gemini.service.js
+++ backend/src/services/gemini.service.js
@@ -1,9 +1,17 @@
 import  {GoogleGenerativeAI} from "@google/generative-ai";   
+import "dotenv/config" 
 
-const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY); 
+const key = process.env.GEMINI_API_KEY
+console.log("---------------------------------------------------------------------------------")
+console.log("Debug da Chave"); 
+console.log("A chave começa com AIza?",key?.startsWith("AIza")); 
+console.log("Tamanho da Chave:",key?.length) 
+console.log("Tem espaços no final?",key !== key?.trim()?"SIM" : "NÃO")  
+console.log("---------------------------------------------------------------------------------")
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
  const model = genAI.getGenerativeModel({
-        model:"gemini-1.0-pro" , 
+        model: "gemini-1.0-pro",  
     }); 
 
 export async function askGemini(prompt){ 
--- backend/src/services/test-gemini.js
+++ backend/src/services/test-gemini.js
@@ -1,12 +1,13 @@
-import {GoogleGenerativeAI} from "@google/generative-ai"; 
-import dotenv from "dotenv"; 
-dotenv.config() ; 
+// import "dotenv/config" 
 
-console.log("KEY:",process.env.GEMINI_API_KEY); 
+// console.log("KEY:",process.env.GEMINI_API_KEY); 
 
-const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY); 
-const model = genAI.getGenerativeModel({model:"gemini-1.0-pro"}); 
+// import {GoogleGenerativeAI} from "@google/generative-ai"; 
 
-const result = await model.generateContent('Explique o arm-lock no jiu-jitsu'); 
-console.log(result.response.text()); 
+// const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY); 
+
+// const model = genAI.getGenerativeModel({model:"gemini-1.5-flash"});  
+
+// const result = await model.generateContent('Explique o arm-lock no jiu-jitsu'); 
+// console.log(result.response.text()); 
 
