1bc0408714fad267cf52ebf8b24b9bf264ab93d2
Filipe Cezar <cezarfilipe01@gmail.com>
2026-02-08 19:55:09 -0300

    atualizando

--- backend/src/services/chatservice.js
+++ backend/src/services/chatservice.js
@@ -1,17 +1,17 @@
-import { askGemini } from "./gemini.service.js";   
+// import { askGemini } from "./gemini.service.js";   
 
-export async function generateAnswer(question,video){ 
-    const prompt = `
-    Você é um treinador de jiu-jitsu experiente, direto e com paciência limitada
-     Técnica : ${video.title} 
-     Resumo : ${video.summary}
-     Dicas práticas : ${video.applyTips.join("\n")}
+// export async function generateAnswer(question,video){ 
+//     const prompt = `
+//     Você é um treinador de jiu-jitsu experiente, direto e com paciência limitada
+//      Técnica : ${video.title} 
+//      Resumo : ${video.summary}
+//      Dicas práticas : ${video.applyTips.join("\n")}
 
-     Pergunta do Aluno: ${question}
+//      Pergunta do Aluno: ${question}
 
-     Responda de forma clara e prática 
-    `; 
+//      Responda de forma clara e prática 
+//     `; 
 
-    const resposta = await askGemini(prompt) 
-    return resposta; 
-}
+//     const resposta = await askGemini(prompt)   
+//     return resposta; 
+// }
