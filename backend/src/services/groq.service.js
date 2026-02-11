adc7466b1cd090c66e272f3433d61abb3f323e34
Filipe Cezar <cezarfilipe01@gmail.com>
2026-02-10 22:27:00 -0300

    atualizando

--- backend/src/services/groq.service.js
+++ backend/src/services/groq.service.js
@@ -2,7 +2,7 @@ import Groq from "groq-sdk";
 
 
 
-export async function askGroq(prompt){
+export async function askGroq(prompt,video){
     if(!process.env.GROQ_API_KEY){
          console.error("A chave da Api não foi encontrada"); 
         throw new Error("GROQ_API_KEY não encontrada") 
@@ -12,17 +12,24 @@ export async function askGroq(prompt){
     apiKey: process.env.GROQ_API_KEY 
     }); 
 
+    const fullPrompt = `Você é um treinador de jiu-jitsu faixa preta e responde questões sobre jiu-jitsu de forma clara e didatica, o aluno está estudando esse video: 
+    ${video.title}, ${video.summary}, ${video.applyTips} 
+    seu objetivo e tirar todas as dúvidas dele focando nesse contéudo  
+    
+    pergunta do aluno: ${prompt} 
+    `
+
     const completion = await groq.chat.completions.create({
         model:"llama-3.1-8b-instant", 
         messages:[
             {
                 role:"system", 
-                content:"Você responde questões sobre jiu-jitsu de forma didatica" 
+                content:`Você responde questões sobre jiu-jitsu de forma didatica`
             }, 
 
             {
                 role:"user", 
-                content:prompt 
+                content:fullPrompt  
             }
         ],
         temperature:0.3
