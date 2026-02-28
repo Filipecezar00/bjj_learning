114b60e87a5c9c0b19cd7926461fbb3beac50496
Filipecezar00 <cezarfilipe01@gmail.com>
2026-02-28 19:38:43 -0300

    atualizando

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,93 +1,145 @@
-import {useState} from "react"; 
-import CategoryCard from "../components/CategoryCard"; 
-import VideoCard from "../components/VideoCard"; 
-import VideoPage from "./VideoPage"; 
-import {videosByCategory} from "../data/videos"
+import { useState } from "react";
+import CategoryCard from "../components/CategoryCard";
+import VideoCard from "../components/VideoCard";
+import VideoPage from "./VideoPage";
+import { videosByCategory } from "../data/videos";
 import Layout from "../components/Layout";
 
-export default function Home(){
+export default function Home() {
+  const categories = Object.keys(videosByCategory);
 
-const categories = Object.keys(videosByCategory); 
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [selectedVideo, setSelectedVideo] = useState(null);
 
-const [selectedCategory,setSelectedCategory] = useState(null);  
-const [selectedVideo,setSelectedVideo] = useState(null);  
+  const [chatHistory, setChatHistory] = useState([]);
+  const [question, setQuestion] = useState("");
+  const [resposta, setResposta] = useState("");
+  const [loading, setLoading] = useState(false);
 
-const [chatHistory,setChatHistory] = useState([]); 
-const [question,setQuestion] = useState("");  
-const [resposta,setResposta] = useState("") 
-const [loading,setLoading] = useState(false) 
+  async function enviarPergunta() {
+    if (!question.trim()) return;
+    setLoading(true);
+    try {
+      const token = localStorage.getItem("token");
+      const response = await fetch("http://localhost:3000/api/chat", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          message: question,
+          video: selectedVideo,
+        }),
+      });
+      const data = await response.json();
 
-async function enviarPergunta(){
-
-if(!question.trim()) return;  
-setLoading(true); 
-try{
-const token = localStorage.getItem("token")
-const response = await fetch("http://localhost:3000/api/chat",{
-    method:"POST",
-    headers:{"Content-Type":"application/json","Authorization":`Bearer ${token}`}, 
-    body:JSON.stringify({
-    message:question,
-    video:selectedVideo, 
-     }), 
- }); 
-const data = await response.json();  
-
-if(data.answer){
-setChatHistory(prev=>[
-...prev, 
-    {
-        question, 
-        answer:data.answer
+      if (data.answer) {
+        setChatHistory((prev) => [
+          ...prev,
+          {
+            question,
+            answer: data.answer,
+          },
+        ]);
+        setResposta(data.answer);
+      }
+      setQuestion("");
+    } catch (error) {
+      console.error("Erro interno: " + error);
     }
-]); 
-setResposta(data.answer); 
-}
-setQuestion(""); 
 
-}catch(error){
-    console.error("O mestre está limpando o tatame, tente novamente em outro momento");  
-}
+    setLoading(false);
+  }
 
-setLoading(false); 
+  function handleCategoryClick(category) {
+    setSelectedCategory(category);
+  }
+  if (selectedVideo) {
+    return (
+      <VideoPage
+        video={selectedVideo}
+        question={question}
+        setQuestion={setQuestion}
+        chatHistory={chatHistory}
+        onBack={() => setSelectedVideo(null)}
+        onAsk={() => enviarPergunta()}
+        loading={loading}
+        resposta={resposta}
+      ></VideoPage>
+    );
+  }
 
-}
+  // Função com elementos e interface do Usuario
 
-
-function handleCategoryClick(category){
-    setSelectedCategory(category); 
-}
-if(selectedVideo){    
-return(
-        <VideoPage video={selectedVideo} question={question} setQuestion={setQuestion} chatHistory={chatHistory}  onBack={()=>setSelectedVideo(null)} onAsk={()=>enviarPergunta()} loading={loading} resposta={resposta}></VideoPage>        
-    )
-}
-
-// Função com elementos e interface do Usuario 
-
-return(
+  return (
     <Layout>
-            <div style={{padding:"40px",borderRadius:"12px", textAlign:"center", marginBottom:"40px"}}>
-                <h1 style={{fontFamily:"sans-serif",marginBottom:"30px",textAlign:"left",fontSize:"2rem"}}>Estudos de Jiu-jitsu</h1>
+      <div
+        style={{
+          padding: "40px",
+          borderRadius: "12px",
+          textAlign: "center",
+          marginBottom: "40px",
+        }}
+      >
+        <h1
+          style={{
+            fontFamily: "sans-serif",
+            marginBottom: "30px",
+            textAlign: "left",
+            fontSize: "2rem",
+          }}
+        >
+          Estudos de Jiu-jitsu
+        </h1>
 
-            <div style={{display:"flex",flexDirection:"row",gap:"100px",alignItems:"center",width:"100%",maxWidth:"1000px",justifyContent:"space-evenly",margin:"10px",padding:"10px"}}>
-                <div style={{display:"flex",flexDirection:"column",gap:"10px",minWidth:"200px"}}>
-                    {categories.map((cat)=>(
-                        <CategoryCard key={cat} name={cat} onclick={()=>handleCategoryClick(cat)}></CategoryCard>
-                    ))}
-                </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            gap: "100px",
+            alignItems: "center",
+            width: "100%",
+            maxWidth: "1000px",
+            justifyContent: "space-evenly",
+            margin: "10px",
+            padding: "10px",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              gap: "10px",
+              minWidth: "200px",
+            }}
+          >
+            {categories.map((cat) => (
+              <CategoryCard
+                key={cat}
+                name={cat}
+                onclick={() => handleCategoryClick(cat)}
+              ></CategoryCard>
+            ))}
+          </div>
 
-             <div style={{display:"flex",alignItems:"flex-end"}}>
-                    {selectedCategory&&(
-                   <div>
-                        <h2>Videos de {selectedCategory}</h2> 
-                        {videosByCategory[selectedCategory].map((video)=>(
-                            <VideoCard key={video.id} title={video.title} level={video.level} onclick={()=>setSelectedVideo(video)}></VideoCard> 
-                        ))}
-                   </div>
-                    )}
-                        </div>
-                </div>
+          <div style={{ display: "flex", alignItems: "flex-end" }}>
+            {selectedCategory && (
+              <div>
+                <h2>Videos de {selectedCategory}</h2>
+                {videosByCategory[selectedCategory].map((video) => (
+                  <VideoCard
+                    key={video.id}
+                    title={video.title}
+                    level={video.level}
+                    onclick={() => setSelectedVideo(video)}
+                  ></VideoCard>
+                ))}
+              </div>
+            )}
+          </div>
         </div>
-</Layout>
-    )}
+      </div>
+    </Layout>
+  );
+}
