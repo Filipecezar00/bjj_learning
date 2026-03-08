8d1cabf73f941187a38ec0b5cf8bdc2d4080d5b3
Filipecezar00 <cezarfilipe01@gmail.com>
2026-03-07 21:03:09 -0300

    atualizando

--- backend/src/middlawares/auth.middleware.js
+++ backend/src/middlawares/auth.middleware.js
@@ -1,10 +1,14 @@
 import jwt from "jsonwebtoken";
-
+import { errorMiddleware } from "./error.middleware.js";
 export function authMiddleware(req, res, next) {
   const authHeader = req.headers.authorization;
 
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
     return res.status(401).json({ error: "Token não fornecido" });
+  }
+
+  if (!/^Bearer$/i.test(scheme)) {
+    throw new errorMiddleware("Token mal formatado", 401);
   }
 
   const token = authHeader.split(" ")[1];
--- backend/src/middlawares/error.middleware.js
+++ backend/src/middlawares/error.middleware.js
@@ -1,13 +1,13 @@
-export function errorMiddleware(err,req,res,next){
-    console.error({
-        message:err.message,
-        stack:err.stack,
-    }); 
+export function errorMiddleware(err, req, res, next) {
+  console.error({
+    message: err.message,
+    stack: err.stack,
+  });
 
-    const statusCode = err.statusCode || 500 
-    const message = err.statusCode ? err.message : "Erro interno no servidor"; 
+  const statusCode = err.statusCode || 500;
+  const message = err.statusCode ? err.message : "Erro interno no servidor";
 
-    return res.status(statusCode).json({
-        error: message, 
-    }); 
+  return res.status(statusCode).json({
+    error: message,
+  });
 }
