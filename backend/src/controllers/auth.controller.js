d84b5c64b0a45f41300fb1b15082e8d7ecb39c30
Filipecezar00 <cezarfilipe01@gmail.com>
2026-03-14 11:24:13 -0300

    atualizando

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,69 +1,94 @@
-import User from "../models/User.js"; 
-import bcrypt from "bcrypt"; 
-import jwt from "jsonwebtoken"; 
-import { z } from "zod"; 
+import User from "../models/User.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { z } from "zod";
 
-const registerSchema= z.object({
-    name: z.string().min(2,'Preencha pelo menos dois caracteres'),
-    email:z.string().email("Formato de E-mail inválido"),
-    password: z.string("Preencha pelo menos 6 caracteres").min(6) 
-}); 
+const registerSchema = z.object({
+  name: z.string().min(2, "Preencha pelo menos dois caracteres"),
+  email: z.string().email("Formato de E-mail inválido"),
+  password: z.string("Preencha pelo menos 6 caracteres").min(6),
+});
 
-export async function register(req,res){
-    const parsed = registerSchema.safeParse(req.body); 
+const generateTokens = (user) => {
+  const accessToken = jwt.sign(
+    { id: user._id },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: "15m" },
+  );
 
-    if(!parsed.success){
-     throw new AppError("Erro ao Enviar dados do registro, por favor tente novamente",400)
-    }
+  const refreshToken = jwt.sign(
+    { id: user._id },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: "7d" },
+  );
 
-    const {name,email,password} = parsed.data; 
+  return { accessToken, refreshToken };
+};
 
-    const existingUser = await User.findOne({email})
+export async function register(req, res) {
+  const parsed = registerSchema.safeParse(req.body);
 
-    if(existingUser){
-     throw new AppError("Por favor cadastre um Email válido",400); 
-    }
+  if (!parsed.success) {
+    throw new AppError(
+      "Erro ao Enviar dados do registro, por favor tente novamente",
+      400,
+    );
+  }
 
-    const hashedPassword = await bcrypt.hash(password,10); 
+  const { name, email, password } = parsed.data;
 
-    const user = await User.create({
-        name,
-        email,
-        password:hashedPassword 
-    });
-    res.status(201).json({message:"Usuário criado com Sucesso"}); 
+  const existingUser = await User.findOne({ email });
 
+  if (existingUser) {
+    throw new AppError("Por favor cadastre um Email válido", 400);
+  }
 
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+  res.status(201).json({ message: "Usuário criado com Sucesso" });
 }
 
-export async function login(req,res){
-        const {email,password} = req.body; 
+export async function login(req, res) {
+  const { email, password } = req.body;
 
-        const user = await User.findOne({email});
+  const user = await User.findOne({ email });
 
-        if(!user){
-            throw new AppError("Credenciais Inválidas",400); 
-        }
+  const { accessToken, refreshToken } = generateTokens(user);
 
-        const isMatch = await bcrypt.compare(password,user.password); 
+  user.refreshToken = refreshToken;
+  await user.save();
 
-        if(!isMatch){
-            throw new AppError("Credenciais Inválidas",400); 
-        }
+  res.json({
+    accessToken,
+    refreshToken,
+    user: { name: user.name, id: user._id },
+  });
 
-        const token = jwt.sign(
-            {id:user._id},
-            process.env.JWT_SECRET, 
-            {expiresIn:"1d"} 
-        ); 
+  if (!user) {
+    throw new AppError("Credenciais Inválidas", 400);
+  }
 
-    res.json({
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    throw new AppError("Credenciais Inválidas", 400);
+  }
+
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
+
+  res.json({
     token,
-    user:{
-                id:user._id,
-                name:user.name,
-                email:user.email
-        }
-    }); 
-
+    user: {
+      id: user._id,
+      name: user.name,
+      email: user.email,
+    },
+  });
 }
