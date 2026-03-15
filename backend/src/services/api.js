dfc6050f65d604451f04e04a117c8e6f701ebafc
Filipecezar00 <cezarfilipe01@gmail.com>
2026-03-15 13:53:47 -0300

    atualizando

---
+++ backend/src/services/api.js
@@ -0,0 +1,45 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://localhost:3000/api",
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+    }
+    try {
+      const refreshToken = localStorage.getItem("refreshToken");
+
+      const res = await axios.post("http://localhost:3000/api/refresh", {
+        refreshToken,
+      });
+
+      if (res.status === 200) {
+        const { accessToken } = res.data;
+        localStorage.setItem("token", accessToken);
+
+        api.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`;
+        return api(originalRequest);
+      }
+    } catch (refreshError) {
+      localStorage.clear();
+      window.location.href = "/login";
+      return Promise.reject(refreshError);
+    }
+    return Promise.reject(error);
+  },
+);
+export default api;
