538b1311f7c5d1dd68f893f67d0452d85def9a4e
Filipecezar00 <cezarfilipe01@gmail.com>
2026-03-22 22:22:50 -0300

    atualizando

---
+++ backend/src/models/Videos.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const VideoSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  url: { type: String, required: true },
+  category: { type: String, required: true },
+  description: { type: String },
+});
+
+export default mongoose.model("Video", VideoSchema);
