347dfe34a0fdb0e760f7a3d6dae99f32c2ca363c
Filipecezar00 <cezarfilipe01@gmail.com>
2026-04-02 13:59:49 -0300

    atualizando

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -6,4 +6,29 @@ const router = express.Router();
 router.post("/register", register);
 router.post("/login", login);
 router.post("/refresh", refresh);
+
+export async function toggleFavorite(req, res) {
+  try {
+    const userId = req.user.id;
+    const { videoId } = req.body;
+
+    const user = await Usuario.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ message: "Usuário não encontrado" });
+    }
+
+    const index = user.favorites.findIndex((fav) => fav.toString() === videoId);
+
+    if (index === -1) {
+      user.favorites.push(videoId);
+    } else {
+      user.favorites.splice(index, 1);
+    }
+    await user.save();
+    res.json({ favorites: user.favorites });
+  } catch (error) {
+    res.status(500).json({ message: "Erro ao Favoritar" });
+  }
+}
 export default router;
