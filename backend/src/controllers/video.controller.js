d6f357d7deec3737f55e5606a7cc25ae4e92c0be
Filipecezar00 <cezarfilipe01@gmail.com>
2026-04-04 22:09:19 -0300

    atualizando

--- backend/src/controllers/video.controller.js
+++ backend/src/controllers/video.controller.js
@@ -1,4 +1,5 @@
 import Video from "../models/Videos.js";
+import User from "../models/User.js";
 
 export const getVideos = async (req, res) => {
   try {
@@ -60,5 +61,35 @@ export const deletarVideo = async (req, res) => {
   } catch (error) {
     console.error("Erro interno no servidor: " + error);
     return res.status(500).send("Erro ao deletar video");
+  }
+};
+
+export const adicionarAoHistorico = async (req, res) => {
+  try {
+    const { videoId } = req.params;
+    const userId = req.user.id;
+
+    const usuarioId = await User.findById(userId);
+
+    if (!usuarioId) {
+      return res.status(404).json({ message: "Usuário não encontrado" });
+    }
+
+    usuarioId.history = usuarioId.history.filter(
+      (id) => String(id) !== String(videoId),
+    );
+
+    usuarioId.history.unshift(videoId);
+
+    if (usuarioId.history.length > 20) {
+      usuarioId.history.pop();
+    }
+
+    await usuarioId.save();
+
+    return res.status(200).send("Historico salvo com Sucesso");
+  } catch (error) {
+    console.error("Erro ao adicionar video no historico!", error);
+    return res.status(500).send("Erro ao adicionar video no historico!");
   }
 };
