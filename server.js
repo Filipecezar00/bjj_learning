7d798ac3f417807dc4aee74cf645a2dbd27240fb
Filipecezar00 <cezarfilipe01@gmail.com>
2026-04-12 13:29:16 -0300

    corrigindo helmet

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(
   cors({
     origin: "*",
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
