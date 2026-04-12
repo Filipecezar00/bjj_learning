a225fc20184745fba66666c0077f5afffb437678
Filipecezar00 <cezarfilipe01@gmail.com>
2026-04-12 13:14:27 -0300

    atualizando

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const app = express();
 app.use(helmet());
 app.use(
   cors({
-    origin: "https://bjj-learning.vercel.app/",
+    origin: "https://bjj-learning.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
